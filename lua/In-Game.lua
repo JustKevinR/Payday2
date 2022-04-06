4d0457161a8fbf5e4595302424bd0d6036987363
JustKevinR <kevin.reinov@gmail.com>
2022-04-06 23:11:24 +0300

    Mod menu #0.3

--- lua/In-Game.lua
+++ lua/In-Game.lua
@@ -1,3 +1,6 @@
+local T_data = tweak_data
+local M_player = managers.player
+
 --------------------------------------------------------------------------------------------
 -- SIMPLE MENU
 if not SimpleMenu then
@@ -56,7 +59,7 @@ end
 ---------------------------------------------------------------------------------------------------------------------------------
 --GLOBALS
 
-function isPlaying()
+ function isPlaying()
 		if not BaseNetworkHandler then return false end
 		return BaseNetworkHandler._gamestate_filter.any_ingame_playing[ game_state_machine:last_queued_state_name() ]
 	end
@@ -76,172 +79,304 @@ local isMultiplayer = isMultiplayer or function()
 
 ----------------------------------------------------------------------------------------------------------------
 -- OPEN MENU
-function openmenu(menu)
+local function openmenu(menu)
         menu:show()
 end
 
-Main_menu = function()
+local function Main_menu()
     openmenu(mymenu_main)
 end
 
-Dodge_menu = function()
+local function Dodge_menu()
     openmenu(mymenu_dodge)
 end
 
-Inventory_menu = function()
-    openmenu(mymenu_inventory)
+local function Inventory_items_menu()
+    openmenu(mymenu_inventory_items)
 end
 
+local function NPC_menu()
+    openmenu(mymenu_npc)
+end
+
+--temp----------------------------------------------
+local function Temp_menu()
+    openmenu(mymenu_temp)
+end
+--temp----------------------------------------------
+
+-- other functions
+local function display_hint(U_hint)
+    managers.hud:show_hint( { text = U_hint .. '!' } )
+end
 ---------------------------------------------------------------------------------------------------------------------------------
 --FUNCTIONS
 
 --inventory functions
-add_keycard = add_keycard or function()
-    managers.player:add_special( { name = "bank_manager_key", silent = true, amount = 1 } )
-    managers.hud:show_hint( { text = "Keycard added!" } )
+local function add_keycard()
+    M_player:add_special( { name = "bank_manager_key", silent = true, amount = 1 } )
+    display_hint("Keycard added")
 end
-add_crowbar = add_crowbar or function()
-    managers.player:add_special( { name = "crowbar", silent = true, amount = 1 } )
-    managers.hud:show_hint( { text = "Crowbar added!" } )
+local function add_crowbar()
+    M_player:add_special( { name = "crowbar", silent = true, amount = 1 } )
+    display_hint("Crowbar added")
 end
-add_planks = add_planks or function()
-    managers.player:add_special( { name = "planks", silent = true, amount = 1 } )
-    managers.hud:show_hint( { text = "Planks added!" } )
+local function add_planks()
+    M_player:add_special( { name = "planks", silent = true, amount = 1 } )
+    display_hint("Planks added")
 end
-add_glassCutter = add_glassCutter or function()
-    managers.player:add_special( { name = "glass_cutter", silent = true, amount = 1 } )
-    managers.hud:show_hint( { text = "Glass cutters added!" } )
+local function add_glassCutter()
+    M_player:add_special( { name = "glass_cutter", silent = true, amount = 1 } )
+    display_hint("Glass cutters added")
 end
-add_gasCan = add_gasCan or function()
-    managers.player:add_special( { name = "gas", silent = true, amount = 1 } )
-    managers.hud:show_hint( { text = "Gas can added!" } )
+local function add_gasCan()
+    M_player:add_special( { name = "gas", silent = true, amount = 1 } )
+    display_hint("Gas can added")
 end
-add_muriaticAcid = add_muriaticAcid or function()
-    managers.player:add_special( { name = "acid", silent = true, amount = 1 } )
-    managers.hud:show_hint( { text = "Muriatic acid added!" } )
+local function add_muriaticAcid ()
+    M_player:add_special( { name = "acid", silent = true, amount = 1 } )
+    display_hint("Muriatic acid added")
 end
-add_hydrogenChlorided = add_hydrogenChlorided or function()
-    managers.player:add_special( { name = "hydrogen_chloride", silent = true, amount = 1 } )
-    managers.hud:show_hint( { text = "Hydrogen chloride added!" } )
+local function add_hydrogenChlorided()
+    M_player:add_special( { name = "hydrogen_chloride", silent = true, amount = 1 } )
+    display_hint("Hydrogen chloride added")
 end
-add_causticSoda = add_causticSoda or function()
-    managers.player:add_special( { name = "caustic_soda", silent = true, amount = 1 } )
-    managers.hud:show_hint( { text = "Caustic soda added!" } )
+local function add_causticSoda()
+    M_player:add_special( { name = "caustic_soda", silent = true, amount = 1 } )
+    display_hint("Caustic soda added")
 end
-add_cookingCombo = add_cookingCombo or function()
-    managers.player:add_special( { name = "caustic_soda", silent = true, amount = 1 } )
-    managers.player:add_special( { name = "acid", silent = true, amount = 1 } )
-    managers.player:add_special( { name = "hydrogen_chloride", silent = true, amount = 1 } )
-    managers.hud:show_hint( { text = "Cooking combo added!" } )
+local function add_cookingCombo()
+    M_player:add_special( { name = "caustic_soda", silent = true, amount = 1 } )
+    M_player:add_special( { name = "acid", silent = true, amount = 1 } )
+    M_player:add_special( { name = "hydrogen_chloride", silent = true, amount = 1 } )
+    display_hint("Cooking combo added")
 end
 
 --dodge functions
-Default = Default or function()   
-    tweak_data.player.damage.DODGE_INIT = 0
-    managers.hud:show_hint( { text = "dodge set to default!" } )
+local function Default()   
+    T_data.player.damage.DODGE_INIT = 0
+    display_hint('dodge set to default')
 end
-five = five or function()   
-    tweak_data.player.damage.DODGE_INIT = 0.5
-    managers.hud:show_hint( { text = "dodge increased by 5%!" } )
+local function five()   
+    T_data.player.damage.DODGE_INIT = 0.5
+    display_hint("dodge increased by 5%")
 end
-ten = ten or function()   
-    tweak_data.player.damage.DODGE_INIT = 0.10
-    managers.hud:show_hint( { text = "dodge increased by 10%!" } )
+local function ten()   
+    T_data.player.damage.DODGE_INIT = 0.10
+    display_hint("dodge increased by 10%")
 end
-fifteen = fifteen or function()   
-    tweak_data.player.damage.DODGE_INIT = 0.15
-    managers.hud:show_hint( { text = "dodge increased by +15%!" } )
+local function fifteen()   
+    T_data.player.damage.DODGE_INIT = 0.15
+    display_hint('dodge increased by +15%')
 end
-twenty = twenty or function()   
-    tweak_data.player.damage.DODGE_INIT = 0.20
-    managers.hud:show_hint( { text = "dodge increased by +20%!" } )
+local function twenty()   
+    T_data.player.damage.DODGE_INIT = 0.20
+    display_hint('dodge increased by +20%')
 end
-thirty = thirty or function()   
-    tweak_data.player.damage.DODGE_INIT = 0.30
-    managers.hud:show_hint( { text = "dodge increased by +30%!" } )
+local function thirty()   
+    T_data.player.damage.DODGE_INIT = 0.30
+    display_hint('dodge increased by +30%')
 end
-forty = forty or function()   
-    tweak_data.player.damage.DODGE_INIT = 0.40
-    managers.hud:show_hint( { text = "dodge increased by +40%!" } )
+local function forty()   
+    T_data.player.damage.DODGE_INIT = 0.40
+    display_hint('dodge increased by +40%')
 end
-fifty = fifty or function()   
-    tweak_data.player.damage.DODGE_INIT = 0.50
-    managers.hud:show_hint( { text = "dodge increased by +50%!" } )
+local function fifty()   
+    T_data.player.damage.DODGE_INIT = 0.50
+    display_hint('dodge increased by +50%')
 end
-sixty = sixty or function()   
-    tweak_data.player.damage.DODGE_INIT = 0.60
-    managers.hud:show_hint( { text = "dodge increased by +60%!" } )
+local function sixty()   
+    T_data.player.damage.DODGE_INIT = 0.60
+    display_hint('dodge increased by +60%')
 end
-ninety = ninety or function()   
-    tweak_data.player.damage.DODGE_INIT = 0.90
-    managers.hud:show_hint( { text = "dodge increased by +90%!" } )
+local function ninety()   
+    T_data.player.damage.DODGE_INIT = 0.90
+    display_hint('dodge increased by +90%')
 end
-hundred = hundred or function()   
-    tweak_data.player.damage.DODGE_INIT = 1
-    managers.hud:show_hint( { text = "dodge increased by +100%!" } )
+local function hundred()   
+    T_data.player.damage.DODGE_INIT = 1
+    display_hint('dodge increased by +100%')
 end
-threeHundred = threeHundred or function()   
-    tweak_data.player.damage.DODGE_INIT = 3
-    managers.hud:show_hint( { text = "You are Neo!" } )
+local function threeHundred()   
+    T_data.player.damage.DODGE_INIT = 3
+    display_hint('You are Neo')
 end
 
+
+--NPC menu
+local function Kill_all_ai()
+    function nukeunit(pawn)
+        local col_ray = { }
+        col_ray.ray = Vector3(1, 0, 0)
+        col_ray.position = pawn.unit:position()
+    
+        local action_data = {}
+        action_data.variant = "explosion"
+        action_data.damage = 10
+        action_data.attacker_unit = managers.player:player_unit()
+        action_data.col_ray = col_ray
+    
+        pawn.unit:character_damage():damage_explosion(action_data)
+    end
+    
+    for u_key,u_data in pairs(managers.enemy:all_civilians()) do
+        nukeunit(u_data)
+    end
+    
+    for u_key,u_data in pairs(managers.enemy:all_enemies()) do
+        u_data.char_tweak.has_alarm_pager = nil
+        nukeunit(u_data)
+    end
+end
+
+local function No_civi_penalty()
+    function MoneyManager.get_civilian_deduction() return 0 end
+    function MoneyManager.civilian_killed() return 0 end
+end
+
+local function No_alarms()
+    if not _setCool then _setCool = CopMovement.set_cool end
+        function CopMovement:set_cool( state, giveaway )
+            if state == true then _setCool(self, state, giveaway) end
+        end
+        if not _setWhisper then _setWhisper = GroupAIStateBase.set_whisper_mode end
+        function GroupAIStateBase:set_whisper_mode( enabled )
+            if enabled == true then _setWhisper(self, true) end
+        end
+        if not _setObjective then _setObjective = CopBrain.set_objective end
+        function CopBrain:set_objective( new_objective )
+            if (not new_objective) or (new_objective.stance ~= "hos" and new_objective.attitude ~= "engage") then _setObjective(self, new_objective) end
+        end
+        if not _setStance then _setStance = CopMovement.set_stance end
+        function CopMovement:set_stance( new_stance_name )
+            if new_stance_name ~= "hos" then _setStance(self, new_stance_name) end
+        end
+        if not _setStanceCode then _setStanceCode = CopMovement.set_stance_by_code end
+        function CopMovement:set_stance_by_code( new_stance_code )
+            if new_stance_code == 1 then _setStanceCode(self, new_stance_code) end
+        end
+        if not _setInteraction then _setInteraction = CopLogicInactive._set_interaction end
+        function CopLogicInactive._set_interaction( data, my_data )
+            data.char_tweak.has_alarm_pager = false
+            _setInteraction(data, my_data)
+        end
+        if not _setAllowFire then _setAllowFire = CopMovement.set_allow_fire end
+        function CopMovement:set_allow_fire( state )
+            if state == false then _setAllowFire(self, state) end
+        end
+        if not _setAllowFireClient then _setAllowFireClient = CopMovement.set_allow_fire_on_client end
+        function CopMovement:set_allow_fire_on_client( state, unit )
+            if state == false then _setAllowFireClient(self, state, unit) end
+        end
+        if not _actionRequest then _actionRequest = CopMovement.action_request end
+        function CopMovement:action_request( action_desc )
+            if action_desc.variant == "run" then return false end
+
+            return _actionRequest(self, action_desc)
+        end
+        function CopMovement:on_suppressed( state ) end
+        function CopLogicBase._get_logic_state_from_reaction( data, reaction ) return "idle" end 
+        function CopLogicIdle.on_alert( data, alert_data ) end 
+        function GroupAIStateBase:on_police_called( called_reason ) end 
+        function GroupAIStateBase:on_police_weapons_hot( called_reason ) end 
+        function GroupAIStateBase:on_gangster_weapons_hot( called_reason ) end 
+        function GroupAIStateBase:on_enemy_weapons_hot( is_delayed_callback ) end 
+        function GroupAIStateBase:add_alert_listener( id, clbk, filter_num, types, m_pos ) end
+        function GroupAIStateBase:criminal_spotted( unit ) end 
+        function GroupAIStateBase:report_aggression( unit ) end 
+        function GroupAIStateBase:propagate_alert( alert_data ) end
+        function GroupAIStateBase:on_criminal_suspicion_progress( u_suspect, u_observer, status ) end
+        function PlayerMovement:on_suspicion( observer_unit, status ) end 
+        function SecurityCamera:_upd_suspicion( t ) end 
+        function SecurityCamera:_sound_the_alarm( detected_unit ) end 
+        function SecurityCamera:_set_suspicion_sound( suspicion_level ) end 
+end
 ---------------------------------------------------------------------------------------------------------------------------------
 --MENUS
 
 --in-game menu
-opts = {}
-opts[#opts+1] = { text = "Dodge", callback = Dodge_menu }
-opts[#opts+1] = { text = "Inventory", callback = Inventory_menu }
-opts[#opts+1] = { text = "", is_cancel_button = true }
-opts[#opts+1] = { text = "", is_cancel_button = true }
-opts[#opts+1] = { text = "CLOSE", is_cancel_button = true, is_focused_button = true}
-mymenu_main = SimpleMenu:new("In-game menu", " ", opts)
+mymenu_main_options = {
+    { text = "Dodge Menu", callback = Dodge_menu },
+    { text = "NPC Menu", callback = NPC_menu},
+    { text = "Inventory Menu", callback = Inventory_items_menu },
+    { text = "", is_cancel_button = true },
+    { text = "", is_cancel_button = true },
+    { text = "CLOSE", is_cancel_button = true, is_focused_button = true}
+}
+mymenu_main = SimpleMenu:new("In-game menu", " ", mymenu_main_options)
 if isPlaying() and inGame() and isMultiplayer() and managers.hud then
 mymenu_main:show()
 
+
 --Inventory menu
-opts = {}
-opts[#opts+1] = {text = 'Add keycard', callback = add_keycard, is_focused_button = true}
-opts[#opts+1] = {text = 'Add crowbar', callback = add_crowbar}
-opts[#opts+1] = { text = "Add planks", callback = add_planks }
-opts[#opts+1] = {text = 'Add glass cutter', callback = add_glassCutter}
-opts[#opts+1] = {text = 'Add gas can', callback = add_gasCan}
-opts[#opts+1] = { text = "", is_cancel_button = true }
-opts[#opts+1] = {text = 'Add muriatic acid', callback = add_muriaticAcid}
-opts[#opts+1] = {text = 'Add hydrogen chloride', callback = add_hydrogenChlorided}
-opts[#opts+1] = {text = 'Add caustic soda', callback = add_causticSoda}
-opts[#opts+1] = { text = "", is_cancel_button = true }
-opts[#opts+1] = {text = 'Cooking combo', callback = add_cookingCombo}
-opts[#opts+1] = { text = "", is_cancel_button = true }
-opts[#opts+1] = { text = "", is_cancel_button = true, }
-opts[#opts+1] = { text = "Back", callback = Main_menu }
-opts[#opts+1] = { text = "CLOSE", is_cancel_button = true}
-mymenu_inventory = SimpleMenu:new("Inventory Menu", "Add stuff to inventory.", opts)
-mymenu_inventory:hide()
+mymenu_inventory_items_options = {
+    {text = 'Add keycard', callback = add_keycard, is_focused_button = true},
+    {text = 'Add crowbar', callback = add_crowbar},
+    { text = "Add planks", callback = add_planks },
+    {text = 'Add glass cutter', callback = add_glassCutter},
+    {text = 'Add gas can', callback = add_gasCan},
+    { text = "", is_cancel_button = true },
+    {text = 'Add muriatic acid', callback = add_muriaticAcid},
+    {text = 'Add hydrogen chloride', callback = add_hydrogenChlorided},
+    {text = 'Add caustic soda', callback = add_causticSoda},
+    { text = "", is_cancel_button = true },
+    {text = 'Cooking combo', callback = add_cookingCombo},
+    { text = "", is_cancel_button = true },
+    { text = "", is_cancel_button = true, },
+    { text = "Back", callback = Main_menu },
+    { text = "CLOSE", is_cancel_button = true}
+}
+mymenu_inventory_items = SimpleMenu:new("Inventory Menu", "Add stuff to inventory.", mymenu_inventory_items_options)
+mymenu_inventory_items:hide()
+
+--NPC menu
+mymenu_npc_options = {
+    { text = "Kill all AI", callback = Kill_all_ai },
+    { text = "No killing penalty", callback = No_civi_penalty },
+    { text = "No alarms & panic", callback = No_alarms },
+    { text = "", is_cancel_button = true },
+    { text = "", is_cancel_button = true },
+    { text = "Back", callback = Main_menu },
+    { text = "CLOSE", is_cancel_button = true}
+}
+mymenu_npc = SimpleMenu:new("NPC menu", " ", mymenu_npc_options)
+mymenu_npc:hide()
 
 --dodge menu
-opts = {}
-opts[#opts+1] = { text = "Default", callback = Default, is_focused_button = true }
-opts[#opts+1] = { text = "", is_cancel_button = true }
-opts[#opts+1] = { text = "+5%", callback = five }
-opts[#opts+1] = { text = "+10%", callback = ten }
-opts[#opts+1] = { text = "+15%", callback = fifteen }
-opts[#opts+1] = { text = "+20%", callback = twenty }
-opts[#opts+1] = { text = "", is_cancel_button = true }
-opts[#opts+1] = { text = "+30%", callback = thirty }
-opts[#opts+1] = { text = "+40%", callback = forty }
-opts[#opts+1] = { text = "+50%", callback = fifty }
-opts[#opts+1] = { text = "+60%", callback = sixty }
-opts[#opts+1] = { text = "+90%", callback = ninety }
-opts[#opts+1] = { text = "+100%", callback = hundred }
-opts[#opts+1] = { text = "", is_cancel_button = true }
-opts[#opts+1] = { text = "Become Neo", callback = threeHundred }
-opts[#opts+1] = { text = "", is_cancel_button = true }
-opts[#opts+1] = { text = "", is_cancel_button = true }
-opts[#opts+1] = { text = "Back", callback = Main_menu }
-opts[#opts+1] = { text = "CLOSE", is_cancel_button = true}
-mymenu_dodge = SimpleMenu:new("Dodge menu", "Increase dodge by...", opts)
+mymenu_dodge_options = {
+    { text = "Default", callback = Default, is_focused_button = true },
+    { text = "", is_cancel_button = true },
+    { text = "+5%", callback = five },
+    { text = "+10%", callback = ten },
+    { text = "+15%", callback = fifteen },
+    { text = "+20%", callback = twenty },
+    { text = "", is_cancel_button = true },
+    { text = "+30%", callback = thirty },
+    { text = "+40%", callback = forty },
+    { text = "+50%", callback = fifty },
+    { text = "+60%", callback = sixty },
+    { text = "+90%", callback = ninety },
+    { text = "+100%", callback = hundred },
+    { text = "", is_cancel_button = true },
+    { text = "Become Neo", callback = threeHundred },
+    { text = "", is_cancel_button = true },
+    { text = "", is_cancel_button = true },
+    { text = "Back", callback = Main_menu },
+    { text = "CLOSE", is_cancel_button = true},
+}
+mymenu_dodge = SimpleMenu:new("Dodge menu", "Increase dodge by...", mymenu_dodge_options)
 mymenu_dodge:hide()
+
+--temp menu-------------------------------------------------------------------------------------------
+mymenu_temp_options = {
+    { text = "", is_cancel_button = true },
+    { text = "", is_cancel_button = true },
+    { text = "Back", callback = Main_menu },
+    { text = "CLOSE", is_cancel_button = true}
+}
+mymenu_temp = SimpleMenu:new("Dodge menu", "Increase dodge by...", mymenu_temp_options)
+mymenu_temp:hide()
+--temp menu end-------------------------------------------------------------------------------------------
 
 --------------------------------------------------------------------------------------
 --In-Game check error
--- lua/Main-Menu.lua
+++ lua/Main-Menu.lua
@@ -1,5 +1,13 @@
 --------------------------------------------------------------------------------------------
--- SIMPLE MENU
+--Shortcuts
+local M_money = managers.money
+local M_xp = managers.experience
+local M_skill = managers.skilltree
+
+
+--------------------------------------------------------------------------------------------
+--SimpleMenu
+
 if not SimpleMenu then
     SimpleMenu = class()
 
@@ -52,23 +60,177 @@ if not SimpleMenu then
     end
 end
 
+
 ----------------------------------------------------------------------------------------------------------------
 -- OPEN MENU
-function openmenu(menu)
+local function openmenu(menu)
     menu:show()
 end
 
-Main_menu = function()
-openmenu(mymenu_main)
+local function Main_menu()
+    openmenu(mymenu_main)
 end
 
----------------------------------------------------------------------------------------------------------------------------------
---MENUS
+local function Level_menu()
+    openmenu(mymenu_level)
+end
 
---in-game menu
-opts = {}
-opts[#opts+1] = { text = "", is_cancel_button = true }
-opts[#opts+1] = { text = "", is_cancel_button = true }
-opts[#opts+1] = { text = "CLOSE", is_cancel_button = true, is_focused_button = true}
-mymenu_main = SimpleMenu:new("In-game menu", " ", opts)
+local function Money_menu()
+    openmenu(mymenu_money)
+end
+
+local function Inventory_menu()
+    openmenu(mymenu_inventory)
+    
+end
+
+
+---------------------------------------------------------------------------------------------------------------------------------
+--FUNCTIONS
+
+--Level functions
+local function level_optionone()
+    M_xp :_set_current_level(0)
+    M_skill:_set_points(0)
+end
+
+local function level_optionsix()
+    M_xp :_set_current_level(5)
+    M_skill:_set_points(5)
+end
+
+local function level_optionseven()
+    M_xp :_set_current_level(10)
+    M_skill:_set_points(12)
+end
+
+local function level_optiontwo()
+    M_xp :_set_current_level(25)
+    M_skill:_set_points(29)
+end
+
+local function level_optionthree()
+    M_xp :_set_current_level(50)
+    M_skill:_set_points(60)
+end
+
+local function level_optionfour()
+    M_xp :_set_current_level(75)
+    M_skill:_set_points(89)
+end
+
+local function level_optionfive()
+    M_xp:_set_current_level(99)
+    M_xp:debug_add_points( 30000000)
+    M_skill:_set_points(120)
+end
+
+
+--money functions
+local function one_four_mil()
+    M_money:_add_to_total(5000000)
+end
+
+local function ten_forty_mil()
+    M_money:_add_to_total(50000000)
+end
+
+local function hundred_four_hundred_mil()
+    M_money:_add_to_total(500000000)
+end
+
+local function one_four_bil()
+    M_money:_add_to_total(5000000000)
+end
+
+local function reset_money()
+    M_money:_deduct_from_total(999999999999999999999)
+    M_money:_deduct_from_offshore(999999999999999999999)
+end
+
+
+--inventory functions
+local function unlock_weapons()
+    local wep_arr = {
+        'new_m4', 'glock_17', 'mp9', 'r870', 'glock_18c', 'amcar', 'm16', 'olympic', 'ak74', 'akm', 'akmsu', 'saiga', 'ak5', 'aug', 'g36', 'p90', 'new_m14', 'deagle', 'new_mp5', 'colt_1911', 'mac10', 'serbu', 'huntsman', 'b92fs', 'new_raging_bull',  'saw'
+    }
+    for i, name in ipairs(wep_arr) do
+        if not managers.upgrades:aquired(name) then
+            managers.upgrades:aquire(name)
+        end
+    end
+end
+
+local function unlock_weapon_mods()
+    for mod_id,_ in pairs(tweak_data.blackmarket.weapon_mods) do
+        tweak_data.blackmarket.weapon_mods[ mod_id ].unlocked = true
+        managers.blackmarket:add_to_inventory("normal", "weapon_mods", mod_id, false)
+    end
+end
+
+local function unlock_masks()
+    managers.blackmarket:_setup_masks()
+    for mask_id,_ in pairs(tweak_data.blackmarket.masks) do
+        Global.blackmarket_manager.masks[mask_id].unlocked = true
+        managers.blackmarket:add_to_inventory("normal", "masks", mask_id, false)
+    end
+end
+---------------------------------------------------------------------------------------------------------------------------------
+--MENU OPTIONS
+
+--Main menu
+mymenu_main_options = {
+    { text = "Level Menu", callback = Level_menu },
+    { text = "Money Menu", callback = Money_menu },
+    { text = "Inventory Menu", callback = Inventory_menu },
+    { text = "", is_cancel_button = true},
+    { text = "", is_cancel_button = true },
+    { text = "CLOSE", is_cancel_button = true, is_focused_button = true}
+}
+mymenu_main = SimpleMenu:new("Main menu", " ", mymenu_main_options)
 mymenu_main:show()
+
+--inventory menu
+mymenu_inventory_options = {
+    { text = "Unlock all weapons", callback = unlock_weapons },
+    { text = "Unlock all weapon mods", callback = unlock_weapon_mods },
+    { text = "", is_cancel_button = true },
+    { text = "", is_cancel_button = true },
+    { text = "Back", callback = Main_menu},
+    { text = "CLOSE", is_cancel_button = true, is_focused_button = true}
+}
+mymenu_inventory = SimpleMenu:new("Inventory Menu", "Unlock items.",mymenu_inventory_options)
+mymenu_inventory:hide()
+
+--Money menu
+mymenu_money_options = {
+    { text = "Add 1 mill cash + 4 mill offshore", callback = one_four_mil},
+    { text = "Add 10 mill cash + 40 mill offshore", callback = ten_forty_mil},
+    { text = "Add 100 mill cash + 400 mill offshore", callback = hundred_four_hundred_mil},
+    { text = "Add 1 billion cash + 4 billion offshore", callback = one_four_bil},
+    { text = "", is_cancel_button = true },
+    { text = "Reset money", callback = reset_money},
+    { text = "", is_cancel_button = true },
+    { text = "", is_cancel_button = true },
+    { text = "Back", callback = Main_menu},
+    { text = "CLOSE", is_cancel_button = true, is_focused_button = true}
+}
+mymenu_money = SimpleMenu:new("Money Menu", "Add/remove cash and offshore funds.", mymenu_money_options)
+mymenu_money:hide()
+
+--Level menu
+mymenu_level_options = {
+    { text = "Set level to 0", callback = level_optionone, is_focused_button = true },
+    { text = "Set level to 5", callback = level_optionsix },
+    { text = "Set level to 10", callback = level_optionseven },
+    { text = "Set level to 25", callback = level_optiontwo },
+    { text = "Set level to 50", callback = level_optionthree },
+    { text = "Set level to 75", callback = level_optionfour },
+    { text = "Set level to 100", callback = level_optionfive },
+    { text = "", is_cancel_button = true },
+    { text = "", is_cancel_button = true },
+    { text = "Back", callback = Main_menu},
+    { text = "CLOSE", is_cancel_button = true, is_focused_button = true}
+}
+mymenu_level = SimpleMenu:new("Level menu", " ", mymenu_level_options)
+mymenu_level:hide()
